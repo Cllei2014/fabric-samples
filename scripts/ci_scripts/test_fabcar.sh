1aa39af6634aa6baffd56d432bacf1df09764745
qianyan <qianyan@thoughtworks.com>
2020-11-17 22:30:58 +0800

    feat: add docker group into docker runtime

--- scripts/ci_scripts/test_fabcar.sh
+++ scripts/ci_scripts/test_fabcar.sh
@@ -2,12 +2,16 @@
 
 SCRIP_PATH=$(readlink -f "$(dirname "$0")")
 
+export UID=$(id -u)
+export GID=$(getent group docker | cut -d ':' -f 3)
+
 docker build --build-arg IMAGE_CA=${IMAGE_CA-hyperledger/fabric-ca-gm:latest} \
              --build-arg IMAGE_TOOLS=${IMAGE_TOOLS-hyperledger/fabric-tools-gm:latest} \
              -t hyperledger/fabric-tools-ca-gm:latest \
              - < $SCRIP_PATH/Dockerfile
 
 docker run --rm \
+    -u $UID:$GID \
     -v "$PWD:$PWD" \
     -v "$(which docker):$(which docker)" \
     -v "$(which docker-compose):$(which docker-compose)" \
