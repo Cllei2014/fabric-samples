7a857ecc49c29a027600e60228df3df8ad62df30
qianyan <qianyan@thoughtworks.com>
2020-11-18 15:30:14 +0800

    chore: minize the sdk dockder runtime

--- scripts/ci_scripts/test_sdk.sh
+++ scripts/ci_scripts/test_sdk.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-GOLANG_VERSION=1.14.12
-CURRENT_USER_ID=$(id -u)
-DOCKER_GROUP_ID=$(getent group docker | cut -d ':' -f 3)
-
-# config for go build
+# cache for go build
 BUILD_DIR=.build
 mkdir -p ${BUILD_DIR}/bin ${BUILD_DIR}/pkg ${BUILD_DIR}/gocache
 
 docker run --rm \
-    -u "$CURRENT_USER_ID:$DOCKER_GROUP_ID" \
     -v "$PWD:$PWD" \
-    -v "$(command -v docker):$(command -v docker)" \
-    -v "$(command -v docker-compose):$(command -v docker-compose)" \
-    -v "/var/run/docker.sock:/var/run/docker.sock" \
     -v "$PWD/${BUILD_DIR}/bin:/opt/gopath/bin" \
     -v "$PWD/${BUILD_DIR}/pkg:/opt/gopath/pkg" \
     -v "$PWD/${BUILD_DIR}/gocache:/opt/gopath/cache" \
     -e "GOCACHE=/opt/gopath/cache" \
     -w "$PWD/fabcar" \
     --network net_byfn \
-    golang:${GOLANG_VERSION} \
+    twblockchain/fabric-baseimage:0.4.22 \
     $1
